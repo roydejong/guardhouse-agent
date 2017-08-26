20889f3d37f3c6d4261953c70f443cc0b74dab29
Roy de Jong <roy@softwarepunt.nl>
2017-08-26 23:22:37 +0200

    [Server] endpoints/index.js: Automatically export all endpoint files

--- net/endpoints/index.js
+++ net/endpoints/index.js
@@ -1,3 +1,29 @@
 module.exports = {
-    "get-status": require('./get-status')      
+    "get-status": require('./get-status')
 };
+
+
+
+
+const path = require('path');
+const fs = require('fs');
+
+let normalizedPath = path.join(__dirname);
+let exportList = { };
+
+fs.readdirSync(normalizedPath).forEach(function (file) {
+    if (file === 'index.js') {
+        return;
+    }
+
+    let subModule = require('./' + file);
+    let subModuleName = file.replace('.js', '');
+
+    exportList[subModuleName] = subModule;
+});
+
+module.exports = exportList;
+
+
+
+
